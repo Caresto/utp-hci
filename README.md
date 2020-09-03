c42fbdbe28aba324d662aa121f231ebca4487b29
Caresto <carestofong@gmail.com>
2020-09-03 16:19:22 -0500

    Adding the first reaction test

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # utp-hci
+
+# This repository will be used to store all the code generated
+# For this class
---
+++ Reaction-Time-Test/index.js
@@ -0,0 +1,58 @@
+values = []
+counter = 0
+
+function changeColor(color) {
+    console.log('here')
+    document.getElementById("rectangle").className = "rectangle "+ color;
+}
+
+function main() {
+    console.log("I AM IN MAIN METHOD")
+    time = calculate_time()
+    start()
+    setTimeout(function() { changeColor('red') }, time) ;
+}
+
+function calculate_time() {
+    let min = 2000
+    let max = 5000
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function start() {
+    startTime = new Date();
+}
+
+function end() {
+    endTime = new Date();
+    let timeDiff = endTime - startTime;
+    let seconds = Math.round(timeDiff);
+    let time_reaction = (seconds-time)
+    values.push(time_reaction)
+    counter++
+    document.getElementById('reaction-text-time').textContent= "Tu tiempo de reacción es de : " + time_reaction + 
+    " mili-segundos. " +  "( " + counter + "/10 )"
+    changeColor('blue')
+    if (counter === 10) {
+        document.getElementById('start').disabled = true;
+        document.getElementById('csv').disabled = false;
+    }
+}
+
+function generateCSV() {
+    const filas = [["Tiempo de Reaccion"], [values.join(',')]]
+    let csvContent = "data:text/csv;charset=utf-8," +
+    filas.map(e => e.join(",")).join("\n")
+    let encodedUri = encodeURI(csvContent);
+    let link = document.createElement("a")
+    link.setAttribute("href", encodedUri)
+    link.setAttribute("download", "time_reactions.csv")
+    document.body.appendChild(link);
+    link.click()
+}
+
+document.addEventListener('keyup', processRegistry => {
+    if (event.code === 'Space') {
+        end()
+      }
+})
